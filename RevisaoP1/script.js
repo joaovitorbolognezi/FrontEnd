912e1abee271bb79e7d6615fd83ffb23961db422
João Vitor Bolognezi <joaovitor.b.portela@gmail.com>
2026-04-03 18:37:32 -0300

    Site pra Revisar conteudo
    Criei este site para revisar os conteudos aprendidos em sala com uso de html, css, js para me preparar para prova 1!

---
+++ RevisaoP1/script.js
@@ -0,0 +1,47 @@
+let carrinho = [];
+
+function adicionarCarrinho(nome){
+ carrinho.push(nome);
+ atualizarCarrinho();
+}
+
+function atualizarCarrinho(){
+ document.getElementById("qtd").innerText = carrinho.length;
+ 
+ let lista = document.getElementById("listaCarrinho");
+
+ if (!lista) return;
+
+ lista.innerHTML = "";
+
+ carrinho.forEach((item,index) => {
+  lista.innerHTML += `
+   <li>
+    ${item}
+    <button onclick="removerItem(${index})">X</button>
+   </li>
+  `;
+ });
+}
+
+function removerItem(index) {
+ carrinho.splice(index,1);
+ atualizarCarrinho();
+}  
+
+function abrirCarrinho() {
+ let div = document.getElementById("carrinho");
+
+ if (div.style.right === "0px") {
+   div.style.right = "-300px";
+ } else {
+   div.style.right = "0px";
+ }
+}
+
+function finalizarPedido(){
+ let mensagem = "Olá, quero pedir: \n" + carrinho.join("\n");
+
+ let url = "https://wa.me/5544999999999?text=" + encodeURIComponent(mensagem);
+ window.open(url, "_blank");
+}
